ddbc7a20064d10565326da62a6b72368d9e06a75
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-05-02 20:07:52 +0200

    27: Make it more reusable

--- 27/default.nix
+++ 27/default.nix
@@ -14,33 +14,38 @@ let
   inherit (pkgs) lib;
   npmlock2nix = pkgs.callPackage npmlock2nixSource { };
 
-  shell = npmlock2nix.v2.shell {
-    src = ./.;
-  };
+  shellWithCustomNodeModules = args: f:
+    let
+      shell = npmlock2nix.v2.shell args;
+      custom_node_modules = f shell.node_modules;
 
-  custom_node_modules = shell.node_modules.overrideAttrs (old: {
+      result = shell.overrideAttrs (final: prev: {
+        buildInputs = lib.filter (package:
+          # Filter out node_modules derivation, identified by nodejs passthru
+          #lib.debug.traceSeqN 2 {
+          #  inherit (package) name passthru;
+          #  hasNodeJs = package ? nodejs;
+          #}
+          ! package ? nodejs
+        ) prev.buildInputs ++ [
+          custom_node_modules
+        ];
+
+        shellHook = ''
+          # FIXME: we should somehow register a GC root here in case of a symlink?
+          ${npmlock2nix.v2.internal.add_node_modules_to_cwd custom_node_modules (args.node_modules_mode or "symlink")}
+        '';
+
+      });
+    in result;
+
+  astro-nm = shellWithCustomNodeModules {
+    src = ./.;
+  } (node_modules: node_modules.overrideAttrs (old: {
     buildPhase = ''
       echo "Build phase successfully changed!"
       exit 1
     '';
-  });
+  }));
 
-  astro-nm = shell.overrideAttrs (final: prev: {
-    buildInputs = lib.filter (package:
-      # Filter out node_modules derivation, identified by nodejs passthru
-      #lib.debug.traceSeqN 2 {
-      #  inherit (package) name passthru;
-      #  hasNodeJs = package ? nodejs;
-      #}
-      ! package ? nodejs
-    ) prev.buildInputs ++ [
-      custom_node_modules
-    ];
-
-    shellHook = ''
-      # FIXME: we should somehow register a GC root here in case of a symlink?
-      ${npmlock2nix.v2.internal.add_node_modules_to_cwd custom_node_modules "symlink"}
-    '';
-
-  });
 in astro-nm
