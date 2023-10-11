5501cdc01f323661b9e7aa669ced75f8f0d0acc6
Yuriy Taraday <yuriy.taraday@tweag.io>
2023-10-11 12:42:44 +0200

    Skeleton for storing all episode metadata in Git
    Add a Nix module to frame episode metadata and a default.nix
    to iterate over all episodes and build up metadata for all of them
    in order, potentially using previous episode data for future ones.
    In future will add code to generate README files for each episode,
    announce them and update their description when needed.

---
+++ episodes/default.nix
@@ -0,0 +1,47 @@
+{
+  nixpkgs ? builtins.getFlake "nixpkgs",
+  system ? builtins.currentSystem,
+  pkgs ? nixpkgs.legacyPackages.${system},
+  lib ? nixpkgs.lib,
+}: let
+  episodes = lib.pipe ./. [
+    builtins.readDir
+    # filter directories
+    (elems: builtins.filter (name: elems.${name} == "directory") (builtins.attrNames elems))
+    # parse all ints in names and sort by them
+    (builtins.sort (l: r: (lib.toIntBase10 l) < (lib.toIntBase10 r)))
+    # collect episode.nix and apply them in order
+    (builtins.foldl' (
+        acc: episode_number: let
+          episode_dir = ./. + "/${episode_number}";
+          episode = lib.evalModules {
+            modules =
+              [
+                {
+                  inherit episode_number;
+                }
+                ./episode-module.nix
+              ]
+              ++ lib.optionals (builtins.pathExists (episode_dir + "/episode.nix")) [
+                (episode_dir + "/episode.nix")
+              ];
+            specialArgs = {
+              inherit episode_dir;
+              previous_episode = acc.prev;
+            };
+          };
+        in {
+          prev = episode;
+          res = acc.res ++ [(lib.nameValuePair "_${episode_number}" episode.config)];
+        }
+      ) {
+        prev = {
+          options.date.isDefined = false;
+        };
+        res = [];
+      })
+    (x: builtins.listToAttrs x.res)
+  ];
+in {
+  inherit episodes;
+}
